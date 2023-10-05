1708411f6142f52112c4eb0b1b4f4d4d3bfff764
Elijah Ngocho <elijahngocho24@gmail.com>
2023-10-05 17:55:57 +0300

    in progress

--- index.js
+++ index.js
@@ -1,7 +1,28 @@
-(function (){
-
-    let screen = document.querySelector(".screen");
+(function () {
+    let screen = document.querySelector("input[name='screen']");
     let buttons = document.querySelectorAll(".btn");
-    let clear = document.querySelector("screen");
-    let screen = document.querySelector("screen");
-})
+    let clear = document.querySelector(".btn-clear");
+    let equals = document.querySelector(".btn-equals");
+
+    buttons.forEach(function (button) {
+        button.addEventListener("click", function (event) {
+            const value = event.target.dataset.num;
+            screen.value += value;
+        });
+    });
+
+    equals.addEventListener("click", function (event) {
+        if (screen.value === "") {
+            screen.value = "Please enter: ";
+        } else {
+            
+                let answer = eval(screen.value);
+                screen.value = answer;
+            
+        }
+    });
+
+    clear.addEventListener("click", function (event) {
+        screen.value = "";
+    });
+})();

a1ca0b277d5fb465a99d36b10e6ac602fed936df
Elijah Ngocho <elijahngocho24@gmail.com>
2023-10-05 16:36:13 +0300

    not completed

---
+++ index.js
@@ -0,0 +1,7 @@
+(function (){
+
+    let screen = document.querySelector(".screen");
+    let buttons = document.querySelectorAll(".btn");
+    let clear = document.querySelector("screen");
+    let screen = document.querySelector("screen");
+})

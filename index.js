45d23d04a2a53c79151f6c49eb48f28d6403e0a3
Elijah Ngocho <elijahngocho24@gmail.com>
2023-12-04 18:09:29 +0300

    designed and built a basic calculator app

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
-(function () {
-    let screen = document.querySelector("input[name='screen']");
-    let buttons = document.querySelectorAll(".btn");
-    let clear = document.querySelector(".btn-clear");
-    let equals = document.querySelector(".btn-equals");
-
-    buttons.forEach(function (button) {
-        button.addEventListener("click", function (event) {
-            const value = event.target.dataset.num;
-            screen.value += value;
-        });
-    });
-
-    equals.addEventListener("click", function (event) {
-        if (screen.value === "") {
-            screen.value = "Please enter: ";
-        } else {
-            
-                let answer = eval(screen.value);
-                screen.value = answer;
-            
-        }
-    });
-
-    clear.addEventListener("click", function (event) {
-        screen.value = "";
-    });
-})();
+function calcNumbers(result){
+    form.displayResult.value=form.displayResult.value+result;
+    
+}
